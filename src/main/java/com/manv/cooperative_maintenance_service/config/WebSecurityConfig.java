760c8a71199ae408bab4207fca3c9c77c44ee59c
Andrew Maznev <andrew.maznev@gmail.com>
2025-01-14 22:40:25 +0300

    Some security configuration in progress.

---
+++ src/main/java/com/manv/cooperative_maintenance_service/config/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.manv.cooperative_maintenance_service.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/auth/login", "/error","/auth/registration").permitAll()
+                        .anyRequest().hasAnyRole("USER", "ADMIN")
+                )
+                .formLogin((form) -> form
+                        .loginPage("/auth/login")
+                        .permitAll()
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/homepage",true)
+                        .failureUrl("/auth/login?error")
+                )
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/auth/login")
+                        .permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password(getPasswordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder () {return new BCryptPasswordEncoder(); }
+}
